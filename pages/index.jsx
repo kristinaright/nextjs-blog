b2add7d04992c9ead633c7813916f2b7c52b041b
kristinaright <rhbcnz95>
2022-02-20 18:42:07 +0300

    added react X/O game

--- pages/index.jsx
+++ pages/index.jsx
@@ -13,11 +13,7 @@ export default function Home({ allPostsData }) {
                 <title>{siteTitle}</title>
             </Head>
             <section className={utilStyles.headingMd}>
-                <p>[Your Self Introduction]</p>
-                <p>
-                    (This is a sample website - you’ll be building a site like this in{' '}
-                    <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-                </p>
+                <p>hi! I`m Kris</p>
             </section>
             <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
                 <h2 className={utilStyles.headingLg}>Blog</h2>
@@ -35,7 +31,10 @@ export default function Home({ allPostsData }) {
                     ))}
                 </ul>
             </section>
-            <Game />
+            <section>
+                <h2 className={utilStyles.headingLg}>Game by react docs</h2>
+                <Game />
+            </section>
         </Layout>
     )
 }
